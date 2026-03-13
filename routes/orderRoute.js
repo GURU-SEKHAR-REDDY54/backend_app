a85718ea99c62db99e7d5d63ead79625616445d7
GURU-SEKHAR-REDDY54 <SekharReddy665343@gmail.com>
2026-03-13 12:05:01 +0530

    updated routes

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,3 +1,8 @@
-// 2 post req 
-// 1  / 
-// 2 / update sttus from pending to complete
+import { placeOrder,showOrders } from "../controllers/orderController.js";
+import express from "express"
+const orderRouter = express.Router()
+
+orderRouter.post("/",placeOrder)
+orderRouter.get("/:email",showOrders)
+
+export default orderRouter
